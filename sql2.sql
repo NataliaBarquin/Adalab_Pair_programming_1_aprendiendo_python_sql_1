3de9b26c6b20b6ede86d0b0c77c3aeb052a4e398
Natalia Barquin <nataliabarquin18@gmail.com>
2023-06-16 14:11:55 +0200

    ejercicio 2 sql

---
+++ sql2.sql
@@ -0,0 +1,52 @@
+CREATE SCHEMA tienda_zapatillas;
+
+USE tienda_zapatillas;
+
+CREATE TABLE zapatillas
+	(id_zapatilla INT AUTO_INCREMENT NOT NULL,
+    modelo VARCHAR(45) NOT NULL,
+    color VARCHAR(45) NOT NULL,
+    PRIMARY KEY (id_zapatilla)
+    );
+    
+CREATE TABLE clientes
+	(id_cliente INT AUTO_INCREMENT NOT NULL,
+    nombre VARCHAR(45) NOT NULL,
+    numero_telefono INT(9) NOT NULL,
+    email VARCHAR(45) NOT NULL,
+    direccion VARCHAR(45) NOT NULL,
+    ciudad VARCHAR(45) NULL,
+    provincia VARCHAR(45) NOT NULL,
+    pais VARCHAR(45) NOT NULL,
+    codigo_postal VARCHAR(45) NOT NULL,
+    PRIMARY KEY (id_cliente)
+    );
+    
+CREATE TABLE empleados
+	(id_empleado INT AUTO_INCREMENT NOT NULL,
+    nombre VARCHAR(45) NOT NULL,
+    tienda VARCHAR(45) NOT NULL,
+    salario INT NULL,
+    fecha_incorporacion DATE NOT NULL,
+    PRIMARY KEY (id_empleado)
+    );
+	
+CREATE TABLE facturas
+	(id_facturas INT AUTO_INCREMENT NOT NULL,
+    numero_factura VARCHAR(45) NOT NULL,
+    fecha DATE NOT NULL,
+    id_cliente INT NOT NULL,
+    id_empleado INT NOT NULL,
+    id_zapatilla INT NOT NULL,
+    PRIMARY KEY (id_facturas),
+    CONSTRAINT `fk_tabla_facturas_clientes1`
+		FOREIGN KEY (`id_cliente`)
+        REFERENCES `tienda_zapatillas`.`clientes` (`id_cliente`),
+	CONSTRAINT `fk_tabla_facturas_tabla_empleados1`
+		FOREIGN KEY (`id_empleado`)
+        REFERENCES `tienda_zapatillas`.`empleados` (`id_empleado`),
+	CONSTRAINT `fk_tabla_facturas_zapatillas1`
+		FOREIGN KEY (`id_zapatilla`)
+        REFERENCES `tienda_zapatillas`.`zapatillas` (`id_zapatilla`)
+        );
+    
