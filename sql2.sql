f1836f205546d64660fcd720241fd615293bf5da
Natalia Barquin <nataliabarquin18@gmail.com>
2023-06-20 17:37:17 +0200

    ejercicio 4 insercion datos

--- sql2.sql
+++ sql2.sql
@@ -65,3 +65,39 @@ CREATE TABLE facturas
     
     ALTER TABLE facturas
     ADD COLUMN factura_total FLOAT NOT NULL;
+    
+    INSERT INTO zapatillas (modelo, color, marca, talla)
+		VALUES ('XQYUN', 'negro', 'Nike', 42),
+        ('UOPMN', 'rosas', 'Nike', 39),
+        ('OPNYT', 'verdes', 'Adidas', 35);
+        
+	INSERT INTO empleados (nombre, tienda, salario, fecha_incorporacion)
+		VALUES ('Laura', 'Alcobendas', 25987, '2010-09-03'),
+        ('María', 'Sevilla', null, '2001-04-11'),
+        ('Esther', 'Oviedo', 30165.98, '2000-11-29');
+        
+	INSERT INTO clientes (nombre, numero_telefono, email, direccion, ciudad, provincia, codigo_postal)
+		VALUES ('Mónica', 1234567289, 'monica@email.com', 'Calle Felicidad', 'Móstoles', 'Madrid', 28176),
+        ('Lorena', '289345678', 'lorena@email.com', 'Calle Alegría', 'Barcelona', 'Barcelona', 12346),
+        ('Carmen', '298463759', 'carmen@email.com', 'Calle del Color', 'Vigo', 'Pontevedra', 23456);
+        
+	INSERT INTO facturas (numero_factura, fecha, id_cliente, id_empleado, id_zapatilla, factura_total)
+		VALUES ('123', '2001-12-11', 1, 2, 1, 54.98),
+        ('1234', '2005-05-23', 3, 1, 1, 89.91),
+        ('12345', '2015-09-18', 3, 3, 2, 76.23);
+    
+    UPDATE zapatillas 
+		SET color = 'amarillas'
+        WHERE id_zapatilla = 2;
+        
+	UPDATE empleados 
+		SET tienda = 'A Coruña'
+        WHERE id_empleado = 1;
+        
+	UPDATE clientes
+		SET numero_telefono = '123456728'
+        WHERE id_cliente = 1;
+
+	UPDATE facturas
+		SET factura_total = 89.91
+        WHERE id_zapatilla = 2;
