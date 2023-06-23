dcb201bd5d7f9d127593e588428fc452ea002358
Natalia Barquin <nataliabarquin18@gmail.com>
2023-06-23 17:17:15 +0200

    ejercicio terminado

--- sql_pair5.sql
+++ sql_pair5.sql
@@ -31,15 +31,49 @@ SELECT *
 	SELECT ship_country
 		FROM orders 
 
- 
 
+ USE Northwind;
+	
+SELECT `product_id`, `product_name`, `unit_price`
+	FROM `products`
+    LIMIT 10;
+    
+SELECT `product_id`, `product_name`, `unit_price`
+	FROM `products`
+    ORDER BY `product_id` DESC
+    LIMIT 10;
+   
+SELECT DISTINCT `order_id`
+	 FROM `order_details`;
+    
+SELECT DISTINCT `order_id` 
+	FROM `order_details`
+    LIMIT 2;
+    
+SELECT `order_id`, (`unit_price` * `quantity`) AS `ImporteTotal`
+	FROM `order_details`
+    ORDER BY `ImporteTotal` DESC
+    LIMIT 3;
+    
+SELECT DISTINCT`order_id`, (`unit_price` * `quantity`) AS `ImporteTotal`
+	FROM `order_details`
+    ORDER BY `ImporteTotal` DESC
+    LIMIT 5 OFFSET 5;
+    
+SELECT `category_name` AS `NombreDeCategoria`
+	FROM `categories`
+    ORDER BY `NombreDeCategoria`;
 
+SELECT DATE_ADD(`shipped_date`, INTERVAL 5 DAY) AS `FechaRetrasada`
+	FROM `orders`;
+        
+SELECT `product_id`, `unit_price`
+	FROM `products`
+    WHERE `product_id` BETWEEN  15 AND 50;
 
-
-
-
-
-
+SELECT `unit_price`
+	FROM `products`
+    WHERE `unit_price` IN (18, 19, 20);
 
 
 
